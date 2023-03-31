da952658f087e40a675d373962308ff8f8d710d9
Andrey Dmitriev <admit8490@gmail.com>
2023-03-31 23:17:56 +0200

    chore axios installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,14 @@
+import axios from "axios";
+import { useEffect } from "react";
+
 function App() {
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await axios('https://opentdb.com/api.php?amount=5&encode=url3986')
+      console.log(data);
+    }
+    fetchData();
+  }, [])
   return (
     <div className="App">      
       <h1>Quiz app will be here</h1>     
